88a416032239adc93a3bf2fb512255d67f5c0b76
nayeon park <pny7211@gmail.com>
2024-05-22 18:45:11 +0900

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Card from "../components/Card";
+
+
 
 
 const Home = () => {
+    const Container = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+   
+  `;
+    const [userInfo, setUserInfo] = useState({
+        avatar: "",
+        email: "",
+        first_name: "",
+        id : 0,
+        last_name: "",
+    });
+
+
+ 
+    useEffect(()=> {
+        axios
+        .get(`https://reqres.in/api/users?page=1&per_page=9`)
+        .then((res) => {
+            setUserInfo(res.data.data);
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+    }, []);
+
     return (
         <> 
-            코드를 추가해주세요
+            <h1>Home Page</h1>
+            <Container>
+                {userInfo.map((user)=>(
+                   < Card
+                    key={user.id}
+                    img={user.avatar}
+                    name={`${user.first_name} ${user.last_name}`}
+                    id={user.id}
+                    email={user.email}
+                    />
+                )
+                )}
+            </Container>
+           
+            <Link to="/menu">메뉴 페이지로 이동</Link>
         </>
     );
 };
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const User = () => {
-    <>
-        코드를 추가해주세요.
-    </>
+    
+    const {userId} = useParams();
+    const [userInfo, setUserInfo] = useState({
+        avatar: "",
+        email: "",
+        first_name: "",
+        id : 0,
+        last_name: "",
+    });
+    
+    useEffect(()=> {
+        axios
+        .get(`https://reqres.in/api/users/${userId}`)
+        .then((res) => {
+            setUserInfo(res.data.data);
+        })
+        .catch((e) => {
+            console.log(e);
+        });
+    }, []);
+        
+
+        return(
+          <>
+          <h1>User Information</h1>
+         
+            <img src={userInfo.avatar} />
+            <h3>email : {userInfo.email}</h3>
+            <h3>
+                {userInfo.first_name} {userInfo.last_name}
+            </h3>
+            </>
+        );
 };
 
 export default User;
